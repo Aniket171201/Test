5d930cf91637cd2796046dced06adfa38eeb5eef
Aniket1712 <kawaleaniket17@gmail.com>
2024-08-28 22:09:06 +0530

    bajaj finserv test

---
+++ PRNDestinationApp/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Good Morning!");
+
+        for (int i = 1; i <= 20; i++) {
+
+            System.out.println(i);
+        }
+    }
+}
